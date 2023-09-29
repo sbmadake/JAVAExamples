0164158f83d58ae306498930dab171b13e693c10
Mr.Shital B.Madake <sbmadake@gmail.com>
2023-09-29 12:33:59 +0530

    Initial Commit.....

---
+++ JAVA_With_Github/src/com/training/org/HelloGithub.java
@@ -0,0 +1,8 @@
+package com.training.org;
+
+public class HelloGithub {
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println("Welcome to JAVA Programming");
+	}
+}
